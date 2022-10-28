774cf2ded068d9275316de0cfc903c10f37c410c
Liencourtz <filipe.ssilva97@gmail.com>
2022-10-28 12:46:34 -0300

    up

--- main.py
+++ main.py
@@ -30,22 +30,13 @@ this.dado = ""
 
 pessoa = Flask(__name__)
 
-@pessoa.route('/', methods=['GET','POST'])
-def cadastrar():
-    if request.method == 'POST':
-        this.nome     = request.form['tNovoNome']
-        this.telefone = request.form['tNovoTelefone']
-        this.endereco = request.form['tNovoEndereco']
-        this.data     = request.form['tNovaData']
-        this.dados    = this.modelo.inserir(this.nome, this.telefone, this.endereco, this.data)
-    return render_template('index.html', titulo="Página Principal", resultado=this.dados)
+@pessoa.route('/')
+def index():
+    return render_template('index.html', titulo="Página Principal")
 
-@pessoa.route('/consultar.html', methods=['GET','POST'])
-def consultarTudo():
-    if request.method == 'POST':
-        this.codigo   = request.form['tNovoCodigo']
-        this.msg = this.modelo.consultar(this.codigo)
-    return render_template('home.html', titulo="Consultar", dados=this.msg)
+@pessoa.route('/home')
+def menu():
+    return render_template('home.html', titulo="Home")
 
 
 if __name__ == '__main__':
