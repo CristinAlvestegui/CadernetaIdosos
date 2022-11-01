507332fd37c51827bdaabc1635a09994fc969aad
Liencourtz <filipe.ssilva97@gmail.com>
2022-11-01 12:25:21 -0300

    up

--- main.py
+++ main.py
@@ -34,83 +34,83 @@ pessoa = Flask(__name__)
 def index():
     return render_template('index.html', titulo="Página Principal")
 
-@pessoa.route('/alergias')
+@pessoa.route('/alergias.html')
 def alergias():
     return render_template('alergias.html', titulo="Alergias")
 
-@pessoa.route('/ambiental')
+@pessoa.route('/ambiental.html')
 def ambiental():
     return render_template('ambiental.html', titulo="Ambiental")
 
-@pessoa.route('/antropometricos')
+@pessoa.route('/antropometricos.html')
 def antropometricos():
     return render_template('antropometricos.html', titulo="Antropometrico")
 
-@pessoa.route('/avalIdoso')
+@pessoa.route('/avalIdoso.html')
 def avalIdoso():
     return render_template('avalIdoso.html', titulo="Avaliação do Idoso")
 
-@pessoa.route('/cadastroCuidador')
+@pessoa.route('/cadastroCuidador.html')
 def cadastroCuidador():
     return render_template('cadastroCuidador.html', titulo="Cadastro Cuidador")
 
-@pessoa.route('/cadastroVES')
+@pessoa.route('/cadastroVES.html')
 def cadastroVES():
     return render_template('cadastroVES.html', titulo="Cadastro VES")
 
-@pessoa.route('/cadIdoso')
+@pessoa.route('/cadIdoso.html')
 def cadIdoso():
     return render_template('cadIdoso.html', titulo="Cadastro do Idoso")
 
-@pessoa.route('/cadSaude')
+@pessoa.route('/cadSaude.html')
 def cadSaude():
     return render_template('cadSaude.html', titulo="Cadastro Saude")
 
-@pessoa.route('/cirurgia')
+@pessoa.route('/cirurgia.html')
 def cirurgias():
     return render_template('cirurgia.html', titulo="Cirurgias")
 
-@pessoa.route('/cntrlDeQuedas')
+@pessoa.route('/cntrlDeQuedas.html')
 def ctrlDeQuedas():
     return render_template('cntrlDeQuedas.html', titulo="Quedas")
 
-@pessoa.route('/cntrlGlicemia')
+@pessoa.route('/cntrlGlicemia.html')
 def cntrlGlicemia():
     return render_template('cntrlGlicemia.html', titulo="Glicemia")
 
-@pessoa.route('/cntrlPressaoArterial')
+@pessoa.route('/cntrlPressaoArterial.html')
 def cntrlPressaoArterial():
     return render_template('cntrlPressaoArterial.html', titulo="Pressão Arterial")
 
-@pessoa.route('/complementar')
+@pessoa.route('/complementar.html')
 def complementar():
     return render_template('complementar.html', titulo="Complementar")
 
-@pessoa.route('/controlePeso')
+@pessoa.route('/controlePeso.html')
 def controlePeso():
     return render_template('controlePeso.html', titulo="Peso")
 
-@pessoa.route('/dorCronica')
+@pessoa.route('/dorCronica.html')
 def dorCronica():
     return render_template('dorCronica.html', titulo="Dores Cronicas")
 
-@pessoa.route('/habitosVida')
+@pessoa.route('/habitosVida.html')
 def habitosVida():
     return render_template('habitosVida.html', titulo="Habitos de Vida")
 
-@pessoa.route('/home')
+@pessoa.route('/home.html')
 def menu():
     return render_template('home.html', titulo="Home")
 
-@pessoa.route('/pessoaRef')
+@pessoa.route('/pessoaRef.html')
 def pessoaRef():
     return render_template('pessoaRef.html', titulo="Pessoas de Referencia")
 
-@pessoa.route('/saudeBucal')
+@pessoa.route('/saudeBucal.html')
 def saudeBucal():
     return render_template('saudeBucal.html', titulo="Saude Bucal")
 
-@pessoa.route('/vacinacao')
+@pessoa.route('/vacinacao.html')
 def vacinacao():
     return render_template('vacinacao.html', titulo="Vacinas")
 
